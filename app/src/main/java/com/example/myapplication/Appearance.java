470fd75384f7a25a9435fbeb52d0a83fbf8d1103
josephgarcia01 <joseph.garcia07@utrgv.edu>
2022-10-26 21:43:40 -0500

    push 10/26/2022.. added slide to notifications page and added a working dark mode

--- app/src/main/java/com/example/myapplication/Appearance.java
+++ app/src/main/java/com/example/myapplication/Appearance.java
@@ -1,15 +1,31 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Appearance extends AppCompatActivity {
 
+    private Button darkToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appearance);
         getSupportActionBar().setTitle("Appearance");
+        darkToggle = findViewById(R.id.button8);
+
+        darkToggle.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        AppCompatDelegate
+                                .setDefaultNightMode(
+                                        AppCompatDelegate
+                                                .MODE_NIGHT_YES);
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/myapplication/Notify.java
+++ app/src/main/java/com/example/myapplication/Notify.java
@@ -1,15 +1,19 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.Switch;
 import android.os.Bundle;
 
 public class Notify extends AppCompatActivity {
+
+    private Switch mySwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notify);
         getSupportActionBar().setTitle("Notifications");
+
+        mySwitch = (Switch) findViewById(R.id.switch1);
     }
 }
